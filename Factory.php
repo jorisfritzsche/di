c0c0bd33609b05f6551b67e46b4996d343956b32
Joris Fritzsche <joris.fritzsche@totalinternetgroup.nl>
2016-03-08 09:29:34 +0100

    initial commit

---
+++ Factory.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace Di;
+
+class Factory
+{
+    /**
+     * Get the requested class with DI applied.
+     *
+     * @param string $className
+     * @param array  $givenArguments
+     *
+     * @return object
+     *
+     * @throws Exception
+     */
+    public function get(string $className, array $givenArguments = [])
+    {
+        /** Check if the requested class can be autoloaded. */
+        if (!class_exists($className)) {
+            throw new Exception("Class {$className} does not exist.");
+        }
+
+        /** Get the ReflectionClass instance for the requested class name. */
+        $reflector = $this->getReflector($className);
+        /** Get the ReflectionArgument instance for the class' constructor, if it has one. */
+        $constructor = $this->getConstructor($reflector);
+
+        /** if no constructor is defined, simply return a new instance of the class. */
+        if (!$constructor) {
+            return $reflector->newInstance();
+        }
+
+        /** If the constructor has not parameters, simply return a new instance of the class. */
+        if ($constructor->getNumberOfParameters() == 0) {
+            return $reflector->newInstance();
+        }
+
+        /** Get the paremeters of the constructor. */
+        $reflectionParameters = $this->getMethodParams($constructor);
+        /** Merge the parameters with the already provided arguments. */
+        $mergedParameters = $this->mergeParams($reflectionParameters, $givenArguments);
+        /** Load the arguments. Please note: this may be recursive. */
+        $loadedParameters = $this->loadParameters($mergedParameters);
+
+        /** return a new instance of the requested class with the loaded arguments. */
+        return $reflector->newInstanceArgs($loadedParameters);
+    }
+
+    /**
+     * Get the ReflectionClass instance for the given class name.
+     *
+     * @param string $className
+     *
+     * @return \ReflectionClass
+     */
+    protected function getReflector(string $className)
+    {
+        return new \ReflectionClass($className);
+    }
+
+    /**
+     * @param \ReflectionClass $reflector
+     *
+     * @return \ReflectionMethod|null
+     */
+    protected function getConstructor(\ReflectionClass $reflector)
+    {
+        return $reflector->getConstructor();
+    }
+
+    /**
+     * @param \ReflectionMethod $reflectionMethod
+     * @return \ReflectionParameter[]
+     */
+    protected function getMethodParams(\ReflectionMethod $reflectionMethod)
+    {
+        $parameters = $reflectionMethod->getParameters();
+
+        return $parameters;
+    }
+
+    /**
+     * @param \ReflectionParameter[] $reflectionParameters
+     * @param mixed[]                $givenArguments
+     *
+     * @return \mixed[]
+     *
+     * @throws Exception
+     */
+    protected function mergeParams(array $reflectionParameters, array $givenArguments)
+    {
+        /** Create an array of parameters. */
+        $mergedParams = [];
+        /** Start by looping through all ReflectionParameters. */
+        foreach ($reflectionParameters as $reflectionParameter) {
+            /** Get the parameter's name. */
+            $name = $reflectionParameter->getName();
+
+            /**
+             * If an argument is given with the same name as the ReflectionParameter's name. Add the argument to the
+             * array instead.
+             */
+            if (isset($givenArguments[$name])) {
+                $mergedParams[$name] = $givenArguments[$name];
+                continue;
+            }
+
+            /** Get the ReflectionType for the given parameter. */
+            $type = $reflectionParameter->getType();
+
+            /** If the type is an auto-loadable class, add it to the array. */
+            if (class_exists($type)) {
+                $mergedParams[$reflectionParameter->getName()] = $reflectionParameter->getType();
+                continue;
+            }
+
+            /** Otherwise, if it has a default value, add it to the array. */
+            if ($reflectionParameter->isDefaultValueAvailable()) {
+                $mergedParams[$reflectionParameter->getName()] = $reflectionParameter->getDefaultValue();
+                continue;
+            }
+
+            /**
+             * N.B. If the ReflectionParameter is not an auto-loadable class and has no default value, it will not be
+             * added to the array. What this means is, that unless the calling function has supplied it in the
+             * givenArguments array, it will lead to an error later on when the class is instantiated with insufficient
+             * parameters.
+             */
+            throw new Exception(
+                "Parameter {$name} cannot be autoloaded, has no default value and was not given as an argument."
+            );
+        }
+
+        return $mergedParams;
+    }
+
+    /**
+     * @param mixed[] $mergedParameters
+     *
+     * @return \object[]
+     *
+     * @throws Exception
+     */
+    protected function loadParameters(array $mergedParameters)
+    {
+        /** Loop through all parameters and load them if possible. */
+        foreach ($mergedParameters as $name => $parameter) {
+            /**
+             * If the parameter can be autoloaded, do so using this class' getter. This way we can recursively inject
+             * dependencies.
+             */
+            if ($parameter instanceof \ReflectionType) {
+                $mergedParameters[$name] = $this->get($parameter);
+            }
+        }
+
+        return $mergedParameters;
+    }
+}
