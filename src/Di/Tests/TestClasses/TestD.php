15d5cf6cd8f6c402fd5ee18ea54a4fc97307da2f
Joris Fritzsche <joris.fritzsche@outlook.com>
2016-03-10 09:49:30 +0100

    added more tests

---
+++ src/Di/Tests/TestClasses/TestD.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @copyright 2016 Joris Fritzsche
+ * @license MIT
+ * @author Joris Fritzsche (joris.fritzsche@outlook.com)
+ */
+
+declare(strict_types = 1);
+
+
+namespace Di\Tests\TestClasses;
+
+
+class TestD extends TestA
+{
+    public $scalarValue;
+
+    public function __construct(string $scalarValue)
+    {
+        $this->scalarValue = $scalarValue;
+    }
+}
